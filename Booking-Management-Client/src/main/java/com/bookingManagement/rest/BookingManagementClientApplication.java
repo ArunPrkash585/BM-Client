4ad74c4c559246177edfc7ac07bfdde6c856be6f
arun <arun@Arun>
2017-12-29 00:01:27 +0530

    InitiAl commit

---
+++ Booking-Management-Client/src/main/java/com/bookingManagement/rest/BookingManagementClientApplication.java
@@ -0,0 +1,12 @@
+package com.bookingManagement.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingManagementClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingManagementClientApplication.class, args);
+	}
+}
